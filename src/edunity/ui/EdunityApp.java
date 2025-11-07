8c309324ab4101c938501479fe9d3d84d7afefd7
cs-nick06 <cs.nick06@gmail.com>
2025-11-07 23:12:21 +0000

    Initial commit

---
+++ src/edunity/ui/EdunityApp.java
@@ -0,0 +1,12 @@
+package edunity.ui;
+
+import java.util.Scanner;
+
+/**
+ * Main application class for the Academix Management System.
+ * Provides a console-based menu interface for managing students, courses, payments, and attendance efficiently.
+ * There is potential to turn it into a Full Stack application"
+ */
+
+public class EdunityApp {
+}
